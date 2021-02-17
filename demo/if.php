99ee42b58f9cca19c5a9c9183b91358fbd413db6
NING-XUHUI <1998ok4nxh@gmail.com>
2021-02-17 23:25:29 +0800

    if

---
+++ demo/if.php
@@ -0,0 +1,20 @@
+<?php
+    // 分支结构
+
+    // 最简if
+    $a = 'Sunday';
+    if ($a == 'Sunday') {
+        echo 'go out';
+    } else {
+        echo 'work';
+    }
+
+    if ($a == 'Sunday') {
+        echo 'go out';
+    } else if ($a == 'Satday') {
+        echo 'go out';
+    } else {
+        echo 'work';
+    }
+
+
